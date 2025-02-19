524ed7c5580fe77fac1b8b2e7f1d7a0548322488
Jakob Ankarhem <jakob@ankarhem.dev>
2025-02-19 14:23:41 +0100

    add cartservice

--- src/app/features/cart/cart.component.ts
+++ src/app/features/cart/cart.component.ts
@@ -9,6 +9,7 @@ import {PlatformAdapterService} from '~/app/core/platform/platform';
 import {BehaviorSubject, combineLatestWith, filter, finalize, map, take} from 'rxjs';
 import {PricePipe} from '~/app/shared/pipes/price.pipe';
 import {SyncService} from '~/app/core/sync/sync.service';
+import {CartService} from '~/app/features/cart/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -22,23 +23,9 @@ import {SyncService} from '~/app/core/sync/sync.service';
   templateUrl: './cart.component.html',
 })
 export class CartComponent {
-  private orderService = inject(OrderService);
-  private platformAdapterService = inject(PlatformAdapterService);
-  private syncService = inject(SyncService);
+  private cartService = inject(CartService);
 
-  private optimisticItem$ = new BehaviorSubject<Item | undefined>(undefined);
-  items$ = this.orderService.order$.pipe(
-    map(order => order.cart?.items),
-    filter(items => !!items),
-    combineLatestWith(this.optimisticItem$),
-    map(([items, optimisticItem]) => {
-      if (!optimisticItem) return items;
-      const idx = items.findIndex(item => item.id === optimisticItem.id);
-      if (typeof idx === 'undefined' || idx === -1) return items;
-      items.splice(idx, 1, optimisticItem);
-      return items;
-    }),
-  );
+  items = this.cartService.items;
 
   onChange(item: Item, quantity: number) {
     const newItem = {...item, quantity};
@@ -46,13 +33,6 @@ export class CartComponent {
       includingVat: newItem.price!.includingVat! * quantity,
       excludingVat: newItem.price!.excludingVat! * quantity,
     };
-    this.optimisticItem$.next(newItem);
-    this.platformAdapterService.updateItem(newItem).pipe(
-      take(1),
-      finalize(() => {
-        this.optimisticItem$.next(undefined);
-        this.syncService.triggerRefresh();
-      })
-    ).subscribe()
+    this.cartService.updateItem$.next(newItem);
   }
 }
---
+++ src/app/features/cart/cart.service.ts
@@ -0,0 +1,40 @@
+import {inject, Injectable, signal} from '@angular/core';
+import {OrderService} from '~/app/core/order/order.service';
+import {PlatformAdapterService} from '~/app/core/platform/platform';
+import {SyncService} from '~/app/core/sync/sync.service';
+import {Item} from '~/openapi/order';
+import {connect} from 'ngxtension/connect';
+import {finalize, Subject, switchMap} from 'rxjs';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  private orderService = inject(OrderService);
+  private platformAdapterService = inject(PlatformAdapterService);
+  private syncService = inject(SyncService);
+
+  private _items = signal<Item[]>([]);
+  items = this._items.asReadonly();
+
+  removeItem$ = new Subject<Item>();
+  updateItem$ = new Subject<Item>();
+
+  constructor() {
+    connect(this._items)
+      .with(this.orderService.order$, (items, order) => order.cart?.items || items)
+      .with(this.removeItem$, (items, item) => items.filter(i => i.id !== item.id))
+      .with(this.updateItem$, (items, item) => {
+        const idx = items.findIndex(i => i.id === item.id);
+        if (idx === -1) return items;
+        items.splice(idx, 1, item);
+        return items;
+      });
+
+    this.updateItem$.pipe(
+      takeUntilDestroyed(),
+      switchMap(item => this.platformAdapterService.updateItem(item)),
+    ).subscribe(() => this.syncService.triggerRefresh());
+  }
+}
